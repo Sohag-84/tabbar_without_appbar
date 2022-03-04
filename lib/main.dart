5d0a8db93802b6eb5be5b1bbc2605a909bedf50c
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-03-04 15:35:31 +0600

    Create tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,13 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>  {
+class _HomePageState extends State<HomePage>  with TickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
+
+    TabController _tabController = TabController(length: 3, vsync: this);
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
@@ -50,6 +53,35 @@ class _HomePageState extends State<HomePage>  {
           Container(
             padding: const EdgeInsets.only(left: 20),
             child: const Text("Discovered",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            child:  TabBar(
+              controller: _tabController,
+              labelColor: Colors.deepPurpleAccent,
+              unselectedLabelColor: Colors.brown,
+              isScrollable: true,
+              indicatorColor: Colors.black26,
+              tabs: const [
+                Tab(text: 'Places',),
+                Tab(text: 'Inspiration',),
+                Tab(text: 'Emotions',),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            width: double.maxFinite,
+            height: 300,
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                Text('data'),
+                Text('data'),
+                Text('data'),
+              ],
+
+            ),
           ),
 
           const SizedBox(height: 20,),
